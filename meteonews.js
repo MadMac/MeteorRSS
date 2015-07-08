961048dfbb9168a3fe00b1ebd42bd0f651505216
Markus Hällfors <markus@hallfors.com>
2015-07-09 00:24:40 +0300

    Read feeds and news

--- meteonews.js
+++ meteonews.js
@@ -1,4 +1,5 @@
 Feeds = new Mongo.Collection("feeds");
+News = new Mongo.Collection("news");
 
 if (Meteor.isClient) {
 
@@ -6,63 +7,125 @@ if (Meteor.isClient) {
     feeds: function() {
       return Feeds.find({});
     }
-  });
 
+  });
+  Template.feed.helpers({
+    news: function(feedId) {
+      console.log(feedId);
+      return News.find({feedId: feedId});
+    }
+  });
   Template.body.events({
     "click .submitFeed": function (event, template) {
       var newUrl = template.find("#newfeed").value;
       if (newUrl != "")
       {
-
         Meteor.call("addFeed", newUrl);
       }
-
       template.find("#newfeed").value = "";
       console.log(event);
 
       return false;
+    },
+    "click .updateFeed": function (event) {
+      Meteor.call("updateFeed", this._id);
 
+      return false;
     }
   });
 }
 
+var addFeed = function(url, meta) {
+  console.log("Lisäys");
+  Fiber(function() {
+    Feeds.insert({
+      url: url,
+      title: meta.title,
+      link: meta.link
+    });
+  }).run();
+};
 
+var addNews = function(title, link, date, feedId) {
+  console.log("Lisäys " + title);
+  Fiber(function() {
+    News.remove({
+      link: link,
+      feedId: feedId
+    });
+    News.insert({
+      link: link,
+      title: title,
+      date: date,
+      feedId: feedId
+    });
+  }).run();
+};
 
 if (Meteor.isServer) {
   var FeedParser = Meteor.npmRequire('feedparser');
   var Request = Meteor.npmRequire('request');
   var Fiber = Meteor.npmRequire('fibers')
-
-  var feedparser = new FeedParser();
-
+  var meta = "";
+  var feedparser;
+  var req;
+  var stream;
   Meteor.methods({
     addFeed:function(url){
-      var req = Request(url);
+      feedparser = new FeedParser();
+      req = Request(url);
 
       req.on('error', function (error) {
         console.log("Error loading url");
       });
       req.on('response', function (res) {
-        var stream = this;
+        stream = this;
 
         stream.pipe(feedparser);
       });
 
+      feedparser.on('error', function(error) {
+        console.log(error, error.stack);
+      });
+      feedparser.on('end', function() {
+
+      });
+      feedparser.on('readable', function() {
+        meta = this.meta;
+        addFeed(url, meta);
+      });
+    
+    },
+    updateFeed:function(feedId) {
+      feedparser = new FeedParser();
+      console.log("Update: " + feedId);
+      var curFeed = Feeds.findOne(feedId);
+
+      req = Request(curFeed.url);
+
+      req.on('error', function (error) {
+        console.log("Error loading url");
+      });
+      req.on('response', function (res) {
+        stream = this;
+        stream.pipe(feedparser);
+      });
 
       feedparser.on('error', function(error) {
         console.log(error, error.stack);
       });
-      feedparser.on('readable', function() {
-        var meta = this.meta;
-        Fiber(function() {
-          Feeds.insert({
-            url: url,
-            title: meta.title,
-            link: meta.link
-          });
-        }).run();
+      feedparser.on('end', function() {
+        console.log("Ended reading feed");
       });
-      //feedparser.parseUrl(url).on('meta', addFeed(url));
+      feedparser.on('readable', function() {
+        var item;
+
+        while (item = this.read())
+        {
+          addNews(item.title, item.link, item.date, feedId);
+        }
+      });
+
     }
   });
 
